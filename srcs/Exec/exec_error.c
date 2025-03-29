dacede4e31ce6a365384a5606ef61250522221ad
Aileen Bonade bottino <aibonade@bess-f2r4s11.clusters.42paris.fr>
2025-03-29 20:04:07 +0100

    exec_error normed

--- srcs/Exec/exec_error.c
+++ srcs/Exec/exec_error.c
@@ -12,13 +12,11 @@
 
 #include "../../includes/minishell.h"
 
-int ft_error_exec(char *perror_str, int ret_val, t_exec *current, int fd_pipe[2])//, int exit)
+int	ft_error_exec(char *perr_str, int ret_val, t_exec *current, int fd_pipe[2])
 {
 	if (ret_val != 130)
-		perror(perror_str);//pas forcement utile perror_str => ici ce sera forcement minipouet
-	//potentiellement des fork deja reussi => wait dans le main_exec
-	//exec a free => dans le main_exec
-	if (current != current->head)//pipe -1 ouvert =>infile de exec
+		perror(perr_str);
+	if (current != current->head)
 	{
 		if (ret_val == ERROR_PIPE)
 		{
@@ -26,11 +24,11 @@ int ft_error_exec(char *perror_str, int ret_val, t_exec *current, int fd_pipe[2]
 			fd_pipe[1] = -1;
 		}
 		if (ft_close(&(current->files->infile->fd), current, fd_pipe) == -1)
-			return(ret_val);//ERROR_CLOSE ?
+			return (ret_val);
 	}
 	if (ret_val == ERROR_FORK || ret_val == ERROR_HEREDOC)
 	{
-		if (current->next)//pipe ouvert (sauf si dernier node)
+		if (current->next)
 		{
 			if (ft_close(&fd_pipe[0], current, fd_pipe) != -1)
 				ft_close(&fd_pipe[1], current, fd_pipe);
@@ -49,29 +47,25 @@ int	ft_put_error_files(t_exec *exec, int ret_val)
 	return (1);
 }
 
-void	ft_error_child(t_exec *exec, int fd_pipe[2], int *fd_to_close, int ret_val)//int ret_value
+void	ft_error_child(t_exec *exec, int fd_pipe[2], int *to_close, int ret_val)
 {
 	t_exec	*exec_head;
 
-	if (ret_val == ERROR_NF)//remplacer str par un code de retour ? si oui, on n'affiche pas sur ERR_NF (-> retourner 127)
+	if (ret_val == ERROR_NF)
 		ret_val = 127;
 	else if (ret_val == ERROR_INFILE || ret_val == ERROR_OUTFILE)
 		ret_val = ft_put_error_files(exec, ret_val);
 	else
-		perror(exec->cmd_array[0]);//A garder ?
+		perror(exec->cmd_array[0]);
 	exec_head = exec->head;
 	if (exec->files->infile->fd > -1)
 		ft_close(&(exec->files->infile->fd), exec_head, fd_pipe);
-	if (fd_to_close && *fd_to_close > -1)
-		ft_close(fd_to_close, exec_head, fd_pipe);
+	if (to_close && *to_close > -1)
+		ft_close(to_close, exec_head, fd_pipe);
 	if (fd_pipe[0] > -1)
-	{
 		ft_close(&fd_pipe[0], exec_head, fd_pipe);
-	}
 	if (fd_pipe[1] > -1)
-	{
 		ft_close(&fd_pipe[1], exec_head, fd_pipe);
-	}
 	ft_lstenvfree(exec_head->infos->env);
 	ft_free_infos(exec_head->infos, 0, 0);
 	free(exec_head->infos);
@@ -79,21 +73,14 @@ void	ft_error_child(t_exec *exec, int fd_pipe[2], int *fd_to_close, int ret_val)
 	exit(ret_val);
 }
 
-void ft_error_close(int fd, t_exec *data, int fd_pipe[2])
+void	ft_error_close(int fd, t_exec *data, int fd_pipe[2])
 {
 	if (fd == fd_pipe[0])
 		fd_pipe[0] = -1;
 	else if (fd == fd_pipe[1])
 		fd_pipe[1] = -1;
 	if (fd_pipe[0] > -1)
-	{
 		ft_close(&fd_pipe[0], data, fd_pipe);
-		//fd_pipe[0] = -1;//plus utile avec ft_close
-	}
 	if (fd_pipe[1] > -1)
-	{
 		ft_close(&fd_pipe[1], data, fd_pipe);
-		//fd_pipe[1] = -1;//plus utile avec ft_close
-	}
-	ft_putstr_fd("Close error.\n", 2);//A virer
 }
